162c8ee8ab2204c0022bca5e79f0b7286da5e460
handsome <1249630552@qq.com>
2020-09-09 17:14:37 +0800

    登录和注册验证规则

--- assets/js/login.js
+++ assets/js/login.js
@@ -12,4 +12,26 @@ $(function() {
         $('.reg-box').hide()
 
     })
+
+    //表单验证
+    //从layui中获取form对象
+    var form = layui.form
+        //通过form.verify()函数自定义校验规则
+    form.verify({
+        pwd: [/^[\S]{6,12}$/,
+            '密码必须6到12位，且不能出现空格'
+        ],
+        //校验量词密码是否一致的规则
+        repwd: function(value) {
+            //通过形参拿到的是确认密码框中的内容
+            //还需要拿到密码框中的内容
+
+            var pwd = $('.reg-box [name=password]').val()
+            if (value != pwd) {
+                return '两次密码不一致'
+            }
+        }
+
+
+    })
 })
